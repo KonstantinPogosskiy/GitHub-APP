9afd597f618b0ab82e483f6c96823df4f8effc89
KonstantinPogosskiy <darcus666@yandex.ru>
2022-05-22 15:06:09 +0300

    rebase icon

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React, {useState} from "react";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import {URL} from "./constants/constants";
+import gitLogo from './assets/frame.svg';
+import glass from './assets/glass.svg';
+import reposIsEmpty from './assets/repos_empty.svg';
+import usersIsEmpty from './assets/users_empty.svg';
+import followers from './assets/followers.svg';
+import following from './assets/following.svg';
 import './App.css';
 
 const App = () => {
@@ -25,19 +31,19 @@ const App = () => {
     return (
         <div className="App">
             <Header
-                logo={'/images/Frame.svg'}
-                find={'/images/glass.svg'}
+                logo={gitLogo}
+                find={glass}
                 getUser={requestUsers}
             />
             <Main
-                reposIsEmpty={'/images/repos_empty.svg'}
-                usersIsEmpty={'/images/users_empty.svg'}
-                find={'/images/glass.svg'}
+                reposIsEmpty={reposIsEmpty}
+                usersIsEmpty={usersIsEmpty}
+                find={glass}
                 isFetching={isFetching}
                 users={data.user}
                 repos={data.repo}
-                followers={'/images/followers.svg'}
-                following={'/images/following.svg'}
+                followers={followers}
+                following={following}
             />
         </div>
     );
